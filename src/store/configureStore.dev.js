541cd5800f7eb91b7e9b8630769db68fb3bf8410
w1nston <wallander.jonas@gmail.com>
2017-01-04 12:24:07 +0100

    Add hot reloading for reducers

--- src/store/configureStore.dev.js
+++ src/store/configureStore.dev.js
@@ -2,10 +2,18 @@ import { createStore } from 'redux';
 import rootReducer from '../reducers';
 
 export default function configureStore(state) {
-  return createStore(
+  const store = createStore(
     rootReducer,
     state,
     window.__REDUX_DEVTOOLS_EXTENSION__ &&
     window.__REDUX_DEVTOOLS_EXTENSION__()
   );
+
+  if (module.hot) {
+    module.hot.accept('../reducers', () =>
+      store.replaceReducer(require('../reducers'))
+    );
+  }
+
+  return store;
 }
