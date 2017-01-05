c5cd41c46957b89fc242a584f572b63723473399
w1nston <wallander.jonas@gmail.com>
2017-01-05 18:16:09 +0100

    Fix forgotten onChange method for <ExercieNameInput/>.

--- src/components/exercises/exerciseNameInput.jsx
+++ src/components/exercises/exerciseNameInput.jsx
@@ -5,11 +5,13 @@ export default function ExerciseNameInput({ input }) {
   const {
     name,
     value,
+    onChange,
   } = input;
   return (
     <TextInput
       name={name}
       value={value}
+      onChange={onChange}
     />
   );
 }
